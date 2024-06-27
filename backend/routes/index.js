48475253a8e829e2671533f58dfd963488127b33
Sahil-Shadwal <sahilshadwal@gmail.com>
2024-06-27 10:25:24 +0530

    keeping main routing file clean and making components

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,32 @@
 const express = require('express');
+const userRouter = require('./user');
 
 const router = express.Router();
 
+router.use("/user",  userRouter);
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //* This follows the Node.js convention of error-first callbacks, where the first argument to the callback is reserved for an error object, and subsequent arguments represent successful response 
 //? The callback function is passed three arguments: the request, the response, and the next middleware function in the application’s request-response cycle.
